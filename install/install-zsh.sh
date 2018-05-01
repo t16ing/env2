9a46faf40d1f15f263eb8fef895391cb0a6335f7
tarlety <tarlety@gmail.com>
2018-05-01 01:46:41 +0000

    local zsh fixes

--- install/install-zsh.sh
+++ install/install-zsh.sh
@@ -8,8 +8,8 @@ chsh
 
 echo "Install oh-my-zsh"
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
-git clone git://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/plugins/zsh-autosuggestions
 sed -i -e 's/ZSH_THEME="robbyrussell"/ZSH_THEME="ys"/g' ~/.zshrc
-sed -i -e 's/plugins=(/plugins=( zsh-autosuggestions/g' ~/.zshrc
+sed -i -e 's/plugins=(/plugins=( zsh-autosuggestions ssh-agent/g' ~/.zshrc
 sed -i -e "s/ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=8'/ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=red'/g" ~/.oh-my-zsh/plugins/zsh-autosuggestions/src/config.zsh
 cd ~/.oh-my-zsh/plugins/zsh-autosuggestions/ ; make
