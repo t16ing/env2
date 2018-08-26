b5131d2df7d4dfbef73da588885e27acd3014dff
t16ing <t16ing@users.noreply.github.com>
2018-08-27 01:04:23 +0800

    zsh theme, mytheme

--- install/install-zsh.sh
+++ install/install-zsh.sh
@@ -9,7 +9,8 @@ chsh
 echo "Install oh-my-zsh"
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/plugins/zsh-autosuggestions
-sed -i -e 's/ZSH_THEME="robbyrussell"/ZSH_THEME="ys"/g' ~/.zshrc
+wget https://raw.githubusercontent.com/t16ing/mytheme.zsh-theme/master/mytheme.zsh-theme -O ~/.oh-my-zsh/themes/mytheme.zsh-theme
+sed -i -e 's/ZSH_THEME="robbyrussell"/ZSH_THEME="mytheme"/g' ~/.zshrc
 sed -i -e 's/^plugins=(/plugins=( zsh-autosuggestions ssh-agent/g' ~/.zshrc
 sed -i -e "s/ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=8'/ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=red'/g" ~/.oh-my-zsh/plugins/zsh-autosuggestions/src/config.zsh
 echo "alias ls='ls --color=tty --group-directories-first'" >> ~/.zshrc
