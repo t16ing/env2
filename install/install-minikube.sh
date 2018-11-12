a5f695943108ec9cb585d0ff684bdd7c57c13c29
t16ing <t16ing@users.noreply.github.com>
2018-11-12 23:12:58 +0800

    update minikube scripts.

---
+++ install/install-minikube.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo apt install virtualbox -y
+sudo snap install kubectl --classic
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/v0.30.0/minikube-linux-amd64 && chmod +x minikube && sudo cp minikube /usr/local/bin/ && rm minikube
+
+minikube version
