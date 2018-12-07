cfcfe5573f0b481ddcee8ca3b8a0ff6fb4eebfc5
t16ing <t16ing@users.noreply.github.com>
2018-12-07 17:27:25 +0800

    update install-minikube

--- install/install-minikube.sh
+++ install/install-minikube.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 
-sudo apt install virtualbox -y
-sudo snap install kubectl --classic
-curl -Lo minikube https://storage.googleapis.com/minikube/releases/v0.30.0/minikube-linux-amd64 && chmod +x minikube && sudo cp minikube /usr/local/bin/ && rm minikube
+# Install kubectl
+# https://kubernetes.io/docs/tasks/tools/install-kubectl/
 
+sudo apt-get update && sudo apt-get install -y apt-transport-https
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt-get install -y kubectl
+
+# Install minikube
+# https://github.com/kubernetes/minikube
+
+sudo apt install virtualbox -y
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 && \
+	sudo install minikube-linux-amd64 /usr/local/bin/minikube
+
+# Installation report
+whereis minikube
 minikube version
+whereis kubectl
+kubectl version
