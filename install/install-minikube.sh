1d9b5a158e4129d303ba1e5cad745d1b424fddae
t16ing <t16ing@users.noreply.github.com>
2018-12-07 17:27:25 +0800

    update install-minikube

--- install/install-minikube.sh
+++ install/install-minikube.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# Install kubectl
+# https://kubernetes.io/docs/tasks/tools/install-kubectl/
+
+sudo apt-get update && sudo apt-get install -y apt-transport-https
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt-get install -y kubectl
+
 # Install minikube
 # https://github.com/kubernetes/minikube
 
@@ -10,3 +19,5 @@ curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-
 # Installation report
 whereis minikube
 minikube version
+whereis kubectl
+kubectl version
