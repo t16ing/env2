32ab72f8e411c9f346850d8aca4549f67b4886d4
tarlety <tarlety@users.noreply.github.com>
2019-12-20 20:19:02 +0800

    install-fonts: add comments for installation purpose.

--- install/install-fonts.sh
+++ install/install-fonts.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+echo "Install nerd fonts for vim nerdtree plugin."
 URL=https://github.com/ryanoasis/nerd-fonts/releases/download/v2.0.0/Ubuntu.zip
 
 mkdir -p ~/.fonts
@@ -7,8 +8,4 @@ cd ~/.fonts
 wget -q -O tmp.zip $URL && unzip tmp.zip && rm tmp.zip
 fc-cache -fv
 cd -
-
-echo For full powerline fonts:
-echo 1. $ git clone https://github.com/powerline/fonts
-echo 2. $ ./install.sh
 
