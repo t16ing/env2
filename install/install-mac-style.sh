327febaa92e4fad0a99b5b4b48aa7a225ed9dabe
tarlety <tarlety@users.noreply.github.com>
2020-03-08 06:16:15 +0800

    add installer for mac style.

--- install/install-mac-style.sh
+++ install/install-mac-style.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+## for mac style spotlight
+
 sudo sh -c "echo 'deb http://download.opensuse.org/repositories/home:/manuelschneid3r/xUbuntu_18.04/ /' > /etc/apt/sources.list.d/home:manuelschneid3r.list"
 curl https://download.opensuse.org/repositories/home:manuelschneid3r/xUbuntu_18.04/Release.key | sudo apt-key add -
 sudo apt-get update
 sudo apt-get install albert
 
+## for windows animation effect
+
+sudo apt-get install compizconfig-settings-manager
