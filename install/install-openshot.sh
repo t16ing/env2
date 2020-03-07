926f744f8b37a771633b696bb8ea2d90b6c86888
tarlety <tarlety@users.noreply.github.com>
2020-03-08 06:27:50 +0800

    Refine installers.

--- install/install-openshot.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# https://www.openshot.org/zh-hant/ppa/
-sudo add-apt-repository ppa:openshot.developers/ppa
-sudo apt-get update
-sudo apt-get install openshot-qt
-
--- install/install-vscode.sh
+++ install/install-vscode.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+## install vscode from microsoft repo
+
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
 sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
 sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
 sudo apt-get install apt-transport-https
 sudo apt-get update
 sudo apt-get install code # or code-insiders
+
+# for large workspace
+
+echo "fs.inotify.max_user_watches=524288" | sudo tee -a /etc/sysctl.conf
+sudo sysctl -p
