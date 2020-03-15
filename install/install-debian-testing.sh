00b94c7b359db6edf87757cbb3410f4aada85a6e
tarlety <tarlety@users.noreply.github.com>
2020-03-15 12:05:58 +0800

    debian testing not working in last commit, remove it.

--- install/install-debian-testing.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cat << EOF | sudo tee -a /etc/apt/sources.list
-## Debian new updated packages available to the user
-# https://serverfault.com/questions/550855/how-to-add-debian-testing-repository-to-apt-get
-deb http://http.us.debian.org/debian/ testing non-free contrib main
-EOF
