2ebde61794a5c35f2c80cf42c4e06579fc8cdf9b
tarlety <tarlety@users.noreply.github.com>
2020-03-17 04:55:09 +0800

    install scripts: gnome-shell extensions.

---
+++ install/install-gnome-shell-extension.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo apt install gnome-tweak-tool
+sudo apt install gnome-shell-extension-hide-activities
+sudo apt install gnome-shell-extension-remove-dropdown-arrows
+sudo apt install chrome-gnome-shell
+
+echo please install: hidetopbar@mathieu.bidon.ca
