93ee73565474781dd4eada9a26dd70296def42db
tarlety <tarlety@users.noreply.github.com>
2020-03-18 19:15:06 +0800

    install scripts: paper icons.

--- install/install-mac-style.sh
+++ install/install-mac-style.sh
@@ -11,4 +11,10 @@ sudo apt-get install albert
 
 sudo add-apt-repository ppa:noobslab/macbuntu
 sudo apt-get update
-sudo apt-get install macbuntu-os-icons-v1804 macbuntu-os-ithemes-v1804
+sudo apt-get install macbuntu-os-icons-v1804 macbuntu-os-ithemes-v1804 -y
+
+## install paper icons, in case you do not like mac icons on ubuntu
+
+sudo add-apt-repository ppa:snwh/ppa
+sudo apt update -y
+sudo apt install paper-icon-theme -y
