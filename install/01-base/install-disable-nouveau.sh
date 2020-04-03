e1057dd7e31f4a6cef8e701510af34260603bc81
tarlety <tarlety@users.noreply.github.com>
2020-04-03 18:17:20 +0800

    new install script to disable unstable nouveau driver.

---
+++ install/01-base/install-disable-nouveau.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+conf="
+blacklist nouveau
+blacklist lbm-nouveau
+options nouveau modeset=0
+alias nouveau off
+alias lbm-nouveau off
+"
+
+echo "${conf}" | sudo tee /etc/modprobe.d/blacklist-nouveau.conf
