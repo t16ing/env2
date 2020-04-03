dc9a64e81bfce162cf0060f27c56bd5e485bada1
tarlety <tarlety@users.noreply.github.com>
2020-04-03 22:10:42 +0800

    install-apps: add gimp.

--- install/01-base/install-apps.sh
+++ install/01-base/install-apps.sh
@@ -7,4 +7,4 @@ sudo apt install python-pip python3-pip -y
 sudo apt install ubuntu-restricted-extras -y
 sudo apt install chromium-browser plank -y
 sudo apt install obs-studio -y
-sudo apt install qalculate -y
+sudo apt install qalculate gimp -y
