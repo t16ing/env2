f01aa323cd8e661db50c8ba7e7b3cf40248b6468
tarlety <tarlety@users.noreply.github.com>
2020-05-08 11:51:43 +0800

    add scripts to install obs from official site.

--- install/01-base/install-apps.sh
+++ install/01-base/install-apps.sh
@@ -6,5 +6,4 @@ sudo apt install curl gpg make -y
 sudo apt install python-pip python3-pip -y
 sudo apt install ubuntu-restricted-extras -y
 sudo apt install chromium-browser plank -y
-sudo apt install obs-studio -y
 sudo apt install qalculate gimp -y
