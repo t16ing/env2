bb11ab7f2b15de4994df006f731ea8a85fe6b7c2
tarlety <tarlety@users.noreply.github.com>
2020-09-30 13:23:08 +0800

    install scripts: do not install chromium for ubuntu 20.

--- install/01-base/install-apps.sh
+++ install/01-base/install-apps.sh
@@ -5,5 +5,5 @@ sudo apt install git tmux vim docker.io -y
 sudo apt install curl gpg make -y
 sudo apt install python-pip python3-pip -y
 sudo apt install ubuntu-restricted-extras -y
-sudo apt install chromium-browser plank -y
+sudo apt install plank -y
 sudo apt install qalculate gimp -y
