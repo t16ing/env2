fbaf6304d5da4b2678af8e7e847e2c0af0445ab3
tarlety <tarlety@users.noreply.github.com>
2020-10-04 12:06:23 +0800

    install script: seperate mac spotlight/theme from paper theme.

--- install/03-customization/install-mac-style.sh
+++ install/03-customization/install-mac-style.sh
@@ -4,17 +4,12 @@
 
 sudo sh -c "echo 'deb http://download.opensuse.org/repositories/home:/manuelschneid3r/xUbuntu_20.04/ /' > /etc/apt/sources.list.d/home:manuelschneid3r.list"
 curl https://build.opensuse.org/projects/home:manuelschneid3r/public_key | sudo apt-key add -
-sudo apt-get update
+sudo apt-get update -y
 sudo apt-get install albert
 
 ## integrted mac themes
 
 sudo add-apt-repository ppa:noobslab/macbuntu
-sudo apt-get update
+sudo apt-get update -y
 sudo apt-get install macbuntu-os-icons-v1804 macbuntu-os-ithemes-v1804 -y
 
-## install paper icons, in case you do not like mac icons on ubuntu
-
-sudo add-apt-repository ppa:snwh/ppa
-sudo apt update -y
-sudo apt install paper-icon-theme -y
