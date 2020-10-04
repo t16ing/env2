802ef8bfa2f2bb6aa07f4c0f780a84f347325227
tarlety <tarlety@users.noreply.github.com>
2020-10-04 12:26:52 +0800

    install script: google chrome add google gpg key.

--- install/01-base/install-google-chrome.sh
+++ install/01-base/install-google-chrome.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# install google gpg key
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+
+# install google chrome repository
 cat << EOF | sudo tee /etc/apt/sources.list.d/google-chrome.list
 deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main
 EOF
