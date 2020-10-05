5f0f38d9f3253d5b8f4bbcaf5aaa985191ba1bf0
tarlety <tarlety@users.noreply.github.com>
2020-10-05 13:05:40 +0800

    install scripts: howdy require fail2cam to run.

--- install/05-optional/install-howdy.sh
+++ install/05-optional/install-howdy.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-echo "NOTICE: assert that gtop and fail2cam is installed"
+echo "NOTICE: fail2cam is required."
 
 sudo add-apt-repository ppa:boltgolt/howdy
 sudo apt update -y
 sudo apt install -y howdy
 
 pam_auth_file="/etc/pam.d/common-auth"
-pam_howdy="auth [success=3 default=ignore] pam_python.so \/lib\/security\/howdy\/pam.py"
+pam_howdy="auth [success=4 default=ignore] pam_python.so \/lib\/security\/howdy\/pam.py"
+pam_fail2cam="auth [default=ignore] pam_exec.so seteuid \/usr\/local\/bin\/fail2cam"
 
-grep howdy ${pam_auth_file} || sudo sed -i "/pam_unix.so nullok_secure/i ${pam_howdy}" ${pam_auth_file}
+grep fail2cam ${pam_auth_file} || {
+  echo "FAILED: fail2cam not found in pam."
+  exit 1
+}
+grep howdy ${pam_auth_file} || sudo sed -i "/pam_unix.so nullok_secure/i ${pam_howdy}\n${pam_fail2cam}" ${pam_auth_file}
