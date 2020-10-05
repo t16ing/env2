2c86562cbfeb575a167e85c2a1267e18abe3fe54
tarlety <tarlety@users.noreply.github.com>
2020-10-05 11:50:43 +0800

    install scripts: howdy ready.

--- install/05-optional/install-gtotp.sh
+++ install/05-optional/install-gtotp.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-#pam_auth_file="/etc/pam.d/common-auth"
-pam_auth_file="common-auth"
+pam_auth_file="/etc/pam.d/common-auth"
 
 pam_gtotp="auth [success=1 default=ignore] pam_google_authenticator.so"
 pam_deny="auth requisite pam_deny.so"
@@ -10,4 +9,4 @@ sudo apt-get install libpam-google-authenticator -y
 
 google-authenticator
 
-sudo grep pam_google_authenticator ${pam_auth_file} || sudo sed -i "/pam_unix.so nullok_secure/i ${pam_gtotp}\n${pam_deny}" ${pam_auth_file}
+grep pam_google_authenticator ${pam_auth_file} || sudo sed -i "/pam_unix.so nullok_secure/i ${pam_gtotp}\n${pam_deny}" ${pam_auth_file}
--- install/05-optional/install-howdy.sh
+++ install/05-optional/install-howdy.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+echo "NOTICE: assert that gtop and fail2cam is installed"
+
 sudo add-apt-repository ppa:boltgolt/howdy
-sudo apt update
-sudo apt install howdy
+sudo apt update -y
+sudo apt install -y howdy
+
+pam_auth_file="/etc/pam.d/common-auth"
+pam_howdy="auth [success=3 default=ignore] pam_python.so \/lib\/security\/howdy\/pam.py"
+
+grep howdy ${pam_auth_file} || sudo sed -i "/pam_unix.so nullok_secure/i ${pam_howdy}" ${pam_auth_file}
