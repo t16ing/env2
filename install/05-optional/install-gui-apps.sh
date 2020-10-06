c50793125f5b4c9f457a16f2ff6175816fd8560e
tarlety <tarlety@users.noreply.github.com>
2020-10-06 09:37:27 +0800

    install scripts: fix: cannot keep lyx docked on plank

--- install/05-optional/install-gui-apps.sh
+++ install/05-optional/install-gui-apps.sh
@@ -7,6 +7,9 @@ sudo apt install -y qalculate
 sudo apt install -y lyx
 sudo apt install -y latex-cjk-chinese
 
+### fix: cannot keep lyx docked on plank
+sudo sed -i 's/StartupWMClass=Lyx/StartupWMClass=lyx/g' /usr/share/applications/lyx.desktop
+
 ## Image Editor
 sudo apt install -y gimp
 sudo apt install -y krita
