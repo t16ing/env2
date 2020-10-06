ce43d65a4cd0698138fba34102866e73989453d7
tarlety <tarlety@users.noreply.github.com>
2020-10-06 19:19:54 +0800

    install script: gnome shell extension for ubuntu 20.

--- install/01-base/install-gnome-shell-extension.sh
+++ install/01-base/install-gnome-shell-extension.sh
@@ -3,6 +3,8 @@
 sudo apt install gnome-tweak-tool
 sudo apt install gnome-shell-extension-hide-activities
 sudo apt install gnome-shell-extension-remove-dropdown-arrows
+sudo apt install gnome-shell-extension-autohidetopbar
+sudo apt install gnome-shell-extension-draw-on-your-screen
+sudo apt remove gnome-shell-extension-appindicator
+sudo apt remove gnome-shell-extension-ubuntu-dock
 sudo apt install chrome-gnome-shell
-
-echo please install: hidetopbar@mathieu.bidon.ca
