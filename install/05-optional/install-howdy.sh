c232bd64b34ffa3e9ef153debb0a2d49638357ee
tarlety <tarlety@users.noreply.github.com>
2020-10-06 13:51:55 +0800

    install scripts: howdy sends message with login status.

--- install/05-optional/install-howdy.sh
+++ install/05-optional/install-howdy.sh
@@ -18,7 +18,8 @@ TGCHATID=\`cat /etc/fail2cam/tg-chatid\`
 
 for IMAGE in /lib/security/howdy/snapshots/*
 do
-  (curl --silent --form chat_id=\${TGCHATID} --form photo="@\${IMAGE}" --form caption="howdy2cam-\${IMAGE}" "https://api.telegram.org/bot\${TGBOTTOKEN}/sendPhoto" | grep -F '{"ok":true,' && rm \${IMAGE}) &
+  CAPTION=\${1:-howdy2cam}-\${IMAGE}
+  (curl --silent --form chat_id=\${TGCHATID} --form photo="@\${IMAGE}" --form caption="\${CAPTION}" "https://api.telegram.org/bot\${TGBOTTOKEN}/sendPhoto" | grep -F '{"ok":true,' && rm \${IMAGE}) &
 done
 
 exit 0
@@ -29,8 +30,8 @@ sudo chmod +x /usr/local/bin/howdy2cam
 
 pam_auth_file="/etc/pam.d/common-auth"
 pam_howdy="auth [success=4 default=ignore] pam_python.so \/lib\/security\/howdy\/pam.py"
-pam_howdy2cam="auth [default=ignore] pam_exec.so seteuid \/usr\/local\/bin\/howdy2cam"
-pam_fail2cam="auth [default=ignore] pam_exec.so seteuid \/usr\/local\/bin\/fail2cam"
+pam_howdy2cam_fail="auth [default=ignore] pam_exec.so seteuid \/usr\/local\/bin\/howdy2cam 'FAILED'"
+pam_howdy2cam_login="auth [default=ignore] pam_exec.so seteuid \/usr\/local\/bin\/howdy2cam 'LOGIN'"
 
 grep fail2cam ${pam_auth_file} || {
   echo "FAILED: fail2cam not found in pam."
