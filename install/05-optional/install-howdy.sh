e78b3329c6cb0e066287eb988cb507ef10b07f1a
tarlety <tarlety@users.noreply.github.com>
2020-10-06 13:44:53 +0800

    install scripts: howdy sends snapshot by fail2cam.

--- install/05-optional/install-howdy.sh
+++ install/05-optional/install-howdy.sh
@@ -2,16 +2,40 @@
 
 echo "NOTICE: fail2cam is required."
 
+## Install howdy package
+
 sudo add-apt-repository ppa:boltgolt/howdy
 sudo apt update -y
 sudo apt install -y howdy
 
+## Install howdy2cam script
+
+cat << EOF | sudo tee /usr/local/bin/howdy2cam
+#!/bin/bash
+
+TGBOTTOKEN=\`cat /etc/fail2cam/tg-bot-token\`
+TGCHATID=\`cat /etc/fail2cam/tg-chatid\`
+
+for IMAGE in /lib/security/howdy/snapshots/*
+do
+  (curl --silent --form chat_id=\${TGCHATID} --form photo="@\${IMAGE}" --form caption="howdy2cam-\${IMAGE}" "https://api.telegram.org/bot\${TGBOTTOKEN}/sendPhoto" | grep -F '{"ok":true,' && rm \${IMAGE}) &
+done
+
+exit 0
+EOF
+sudo chmod +x /usr/local/bin/howdy2cam
+
+## Configure pam for howdy and howdy2cam
+
 pam_auth_file="/etc/pam.d/common-auth"
 pam_howdy="auth [success=4 default=ignore] pam_python.so \/lib\/security\/howdy\/pam.py"
+pam_howdy2cam="auth [default=ignore] pam_exec.so seteuid \/usr\/local\/bin\/howdy2cam"
 pam_fail2cam="auth [default=ignore] pam_exec.so seteuid \/usr\/local\/bin\/fail2cam"
 
 grep fail2cam ${pam_auth_file} || {
   echo "FAILED: fail2cam not found in pam."
   exit 1
 }
-grep howdy ${pam_auth_file} || sudo sed -i "/pam_unix.so nullok_secure/i ${pam_howdy}\n${pam_fail2cam}" ${pam_auth_file}
+sudo sed -i "/pam_unix.so nullok_secure/i ${pam_howdy}\n${pam_howdy2cam}" ${pam_auth_file}
+sudo sed -i "/pam_permit.so/i ${pam_howdy2cam}" ${pam_auth_file}
+
