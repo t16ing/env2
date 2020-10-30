bc9183313765ea56e8ab4b8ebfe6d893b2faa8c4
t16ing <t16ing@users.noreply.github.com>
2020-10-31 03:20:40 +0800

    install scripts: update python/node.js package manager

--- install/01-base/install-apps-base.sh
+++ install/01-base/install-apps-base.sh
@@ -3,5 +3,5 @@
 sudo apt update -y
 sudo apt install git tmux vim docker.io -y
 sudo apt install curl gpg make -y
-sudo apt install python-pip python3-pip -y
+sudo apt install python-pip-whl npm -y
 sudo apt install ubuntu-restricted-extras -y
