d5d8ebd0e75664d8a0aafa4d10490232ecdf8842
t16ing <t16ing@users.noreply.github.com>
2020-11-10 20:08:48 +0800

    [fix] default tmux plugin manager installation.

--- install/05-optional/install-tmux-plugins.sh
+++ install/05-optional/install-tmux-plugins.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 echo "Install tmux plugins"
-sudo apt install -y tmux-plugin-manager || \
-    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+sudo apt install -y tmux-plugin-manager \
+    && ln -s /usr/share/tmux-plugin-manager ~/.tmux/plugins/tmux \
+    || git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 echo "Install tmux plugins dependency"
 sudo apt install -y xsel
