d00b03daa06fc11ac79bab5535df6a66480b75f7
t16ing <t16ing@users.noreply.github.com>
2020-11-16 02:18:27 +0800

    [feat] Patch $PATH and libxft2 for st.

--- install/03-customization/install-fonts.sh
+++ install/03-customization/install-fonts.sh
@@ -6,6 +6,9 @@ sudo apt install fonts-cns11643-kai fonts-cns11643-sung
 echo "Install powerline fonts"
 sudo apt install fonts-powerline
 
+echo "Patch libxft2 to fix crash while rendering color emoji"
+sudo dpkg -i ./libxft2-patched/libxft2_2.3.2-1_amd64.deb
+
 echo "Install nerd fonts for vim nerdtree plugin."
 for URL in \
     https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Ubuntu.zip \
