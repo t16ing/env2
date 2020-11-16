3b85d0f96c0002b9832af7f83a66bea15466d247
t16ing <t16ing@users.noreply.github.com>
2020-11-16 23:22:06 +0800

    [fix] Complete color emoji by google noto font.

--- install/03-customization/install-fonts.sh
+++ install/03-customization/install-fonts.sh
@@ -22,5 +22,16 @@ do
     cd -
 done
 
+echo "Install Google Noto Font to complete emoji."
+# [ref] https://dwm.suckless.narkive.com/yEOaU0RO/dev-xft-bug-with-emoji
+for URL in \
+    https://noto-website-2.storage.googleapis.com/pkgs/Noto-hinted.zip
+do
+    mkdir -p ~/.fonts/google
+    cd ~/.fonts/google
+    wget -q -O tmp.zip $URL && unzip -o tmp.zip && rm tmp.zip
+    cd -
+done
+
 fc-cache -fv
 
