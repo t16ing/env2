e7cba8c8e66b5ed1c75405c04adc3e2b1ebf19cc
tarlety <tarlety@gmail.com>
2021-04-29 16:14:13 +0800

    [feat] complete xmr gui scripts.

--- install/04-hack/install-xmrgui.sh
+++ install/04-hack/install-xmrgui.sh
@@ -8,3 +8,6 @@ docker build --tag monero:build-env-linux --build-arg THREADS=2 --file Dockerfil
 docker run --rm -it -v $(pwd):/monero-gui -w /monero-gui monero:build-env-linux \
     sh -c 'apt install ccache ; git submodule update --init --force --recursive ; make release-static -j2'
 
+mkdir -p ~/local/opt/xmrig/bin
+cp build/release/bin/monero-wallet-gui ~/local/opt/xmrig/bin
+
