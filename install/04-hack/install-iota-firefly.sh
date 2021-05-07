75771a88bd23c7c2a42007d2161cea3ec1b58483
t16ing <t16ing@users.noreply.github.com>
2021-05-07 21:59:46 +0800

    [feat] mkapplink to create gnome application shortcut. [feat] add dogecoin install script. [feat] rust/cargo environment and iota firefly desktop install script. [feat] complete xmr gui scripts.

---
+++ install/04-hack/install-iota-firefly.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+## Dev dependencies
+sudo apt install clang libsecret-1-dev libssl-dev libusb-1.0-0-dev libudev-dev
+
+## Rust is required
+curl https://sh.rustup.rs -sSf | sh
+
+## build and install firefly
+mkdir -p ~/local/opt/iota/src
+cd ~/local/opt/iota/src
+git clone https://github.com/iotaledger/firefly
+
+cd firefly
+yarn
+
+cd packages/backend/bindings/node
+yarn
+cd -
+
+cd packages/desktop
+yarn build
+yarn compile:linux
+cp out/firefly-desktop-1.1.0.AppImage ~/local/opt/iota/bin/firefly-desktop
+cd -
+
+mkdir -p ~/local/opt/iota/bin
+
