45d28e4703c7cd063cd342f69d608e0ed47259e8
tarlety <tarlety@gmail.com>
2023-03-29 17:33:44 +0800

    [fix] missing tmux plugins folder. [feat] Steps for mac style customization.

--- install/01-base/install-tmux.sh
+++ install/01-base/install-tmux.sh
@@ -3,6 +3,7 @@
 sudo apt install -y tmux
 
 echo "Install tmux plugin manger"
+mkdir -p ~/.tmux/plugins
 sudo apt install -y tmux-plugin-manager \
     && ln -s /usr/share/tmux-plugin-manager ~/.tmux/plugins/tpm
 
